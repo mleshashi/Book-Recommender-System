480f37562bf7b400d2b2e851aa153790ce515a74
mleshashi <ss139529@gmail.com>
2025-04-20 23:36:57 +0200

    added the setup.py and the recommendation and poster fetch function in the src/recommend.py

---
+++ src/recommend.py
@@ -0,0 +1,41 @@
+import pickle
+import numpy as np
+
+model = pickle.load(open('resources/model.pkl','rb'))
+book_names = pickle.load(open('resources/book_names.pkl','rb'))
+final_rating = pickle.load(open('resources/final_rating.pkl','rb'))
+book_pivot = pickle.load(open('resources/book_pivot.pkl','rb'))
+
+
+def fetch_poster(suggestion):
+    book_name = []
+    ids_index = []
+    poster_url = []
+
+    for book_id in suggestion:
+        book_name.append(book_pivot.index[book_id])
+
+    for name in book_name[0]: 
+        ids = np.where(final_rating['title'] == name)[0][0]
+        ids_index.append(ids)
+
+    for idx in ids_index:
+        url = final_rating.iloc[idx]['image_url']
+        poster_url.append(url)
+
+    return poster_url
+
+
+
+def recommend_book(book_name):
+    books_list = []
+    book_id = np.where(book_pivot.index == book_name)[0][0]
+    distance, suggestion = model.kneighbors(book_pivot.iloc[book_id,:].values.reshape(1,-1), n_neighbors=6 )
+
+    poster_url = fetch_poster(suggestion)
+    
+    for i in range(len(suggestion)):
+            books = book_pivot.index[suggestion[i]]
+            for j in books:
+                books_list.append(j)
+    return books_list , poster_url       
